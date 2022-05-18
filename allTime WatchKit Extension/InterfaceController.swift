9b16ecc5f21e578e42afedfe3eff8e0c37eaa48a
@a.makarevskiy <a.makarevskiy@infosystems.uz>
2022-05-18 10:08:21 +0500

    Initial Commit

---
+++ allTime WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  allTime WatchKit Extension
+//
+//  Created by Aleksandr on 18/05/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
